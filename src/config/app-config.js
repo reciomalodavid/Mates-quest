90d7f0129c141cc031bd003109c2feffbcc87e73
reciomalodavid <reciomalodavid@gmail.com>
2026-08-02 11:22:39 +0200

    chore: add centralized beta application config

---
+++ src/config/app-config.js
@@ -0,0 +1,12 @@
+window.MATES_QUEST_CONFIG = Object.freeze({
+  appName: 'Mates Quest Beta',
+  shortName: 'MQ Beta',
+  environment: 'beta',
+  version: '1.0.0-beta.1',
+  buildDate: '2026-08-02T10:20:00+01:00',
+  gitCommit: 'pending',
+  storagePrefix: 'matesQuestBeta',
+  indexedDbName: 'matesQuestBeta',
+  cachePrefix: 'mates-quest-beta',
+  firebaseNamespace: 'beta'
+});
